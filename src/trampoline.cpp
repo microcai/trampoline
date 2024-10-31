cd504fd57917c538ff4a1a3fc1e7671023d759d6
microcai <microcaicai@gmail.com>
2024-10-31 08:53:48 +0800

    this might help macos ?

--- src/trampoline.cpp
+++ src/trampoline.cpp
@@ -5,6 +5,18 @@ extern "C" void trampoline_entry_point();
 
 const unsigned char* trampoline::_machine_code_template()
 {
+	#if defined (__aarch64__)
+	static constinit uint32_t arm64_trampoline_code[] = {
+		0x1000000a,
+		0x58000069,
+		0xd61f0120,
+		0xd503201f
+	};
+
+	return (const unsigned char*) arm64_trampoline_code;
+
+	#else
 	auto address = &trampoline_entry_point;
 	return (const unsigned char*) address;
+	#endif
 }
